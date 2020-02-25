70fa539c72048187dc1fee506689a5b052b6b896
unknown <jiakai@bupt.edu.cn>
2020-02-26 03:02:14 +0800

    Updates

---
+++ precache-manifest.8de26d83fd25b2b7c510a45dbcb5ec8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af55ed024f598dc592c455a91068a7a",
+    "url": "/myReactApp/index.html"
+  },
+  {
+    "revision": "8255434d64feba7e9edc",
+    "url": "/myReactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "08d526b54226f1575b75",
+    "url": "/myReactApp/static/js/2.d9d85f98.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/myReactApp/static/js/2.d9d85f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8255434d64feba7e9edc",
+    "url": "/myReactApp/static/js/main.d0fb2dd5.chunk.js"
+  },
+  {
+    "revision": "7bab7c1563aeed96fab6",
+    "url": "/myReactApp/static/js/runtime-main.b777ec76.js"
+  }
+]);
